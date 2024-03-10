016dd42fd45d8a391495bbc33d30f301d4b1ec7a
SRenuuu <sandulrenuja@gmail.com>
2024-03-10 11:26:38 +0530

    Add basic theme, assets and main screens

---
+++ flutter_app/lib/app_theme.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'constants.dart';
+
+class AppTheme {
+  static ThemeData getTheme() {
+    final ThemeData base = ThemeData.light();
+
+    return base.copyWith(
+      primaryColor: AppColors.primaryColor,
+      hintColor: AppColors.accentColor,
+      scaffoldBackgroundColor: Colors.white,
+      splashColor: AppColors.accentColor,
+      highlightColor: AppColors.accentColor,
+      cardColor: Colors.white,
+      textSelectionTheme: const TextSelectionThemeData(
+        selectionHandleColor: AppColors.primaryColor,
+        cursorColor: AppColors.primaryColor,
+        selectionColor: AppColors.accentColor,
+      ),
+      // textTheme: _buildTextTheme(base.textTheme),
+      primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+      inputDecorationTheme: _buildInputDecorationTheme(
+        base.inputDecorationTheme,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: AppColors.primaryColor,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(double.infinity, 50),
+          textStyle: const TextStyle(fontSize: 17.0),
+          foregroundColor: Colors.white,
+          backgroundColor: AppColors.primaryColor,
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          minimumSize: const Size(double.infinity, 50),
+          textStyle: const TextStyle(fontSize: 17.0),
+          foregroundColor: AppColors.primaryColor,
+          side: const BorderSide(
+            color: AppColors.primaryColor,
+          ),
+        ),
+      ),
+      tabBarTheme: TabBarTheme(
+        indicatorColor: AppColors.primaryColor,
+        labelColor: AppColors.primaryColor,
+        unselectedLabelColor: Colors.grey,
+        overlayColor:
+            MaterialStateProperty.all(AppColors.primaryColor.withOpacity(0.1)),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      appBarTheme: AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        color: AppColors.primaryColor.withOpacity(0.05),
+      ),
+    );
+  }
+
+  static TextTheme _buildTextTheme(TextTheme base) {
+    return base.copyWith(
+      displayLarge: base.displayLarge?.copyWith(color: AppColors.primaryColor),
+      headlineLarge:
+          base.headlineLarge?.copyWith(color: AppColors.primaryColor),
+      titleLarge: base.titleLarge?.copyWith(color: AppColors.primaryColor),
+      bodyLarge: base.bodyLarge?.copyWith(color: AppColors.primaryColor),
+      labelLarge: base.labelLarge?.copyWith(color: AppColors.primaryColor),
+    );
+  }
+
+  static InputDecorationTheme _buildInputDecorationTheme(
+      InputDecorationTheme base) {
+    return base.copyWith(
+      floatingLabelStyle: const TextStyle(color: AppColors.primaryColor),
+      focusedBorder: const OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        borderSide: BorderSide(
+          width: 2.0,
+          color: AppColors.primaryColor,
+        ),
+      ),
+      enabledBorder: const OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        borderSide: BorderSide(
+          width: 2.0,
+          color: AppColors.greyColor,
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_app/lib/views/auth/sign_in_view.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      child: Image.asset(
+                        'assets/images/workwise-logo.png',
+                        width: 170.0,
+                      ),
+                    ),
+                  ),
+                  const Text("Welcome back!", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 40.0),
+                  // Email Field
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                    ),
+                  ),
+                  const SizedBox(height: 24.0), // Spacer
+                  // Password Field
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(height: 8.0), // Spacer
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          print('Forgot Password button pressed');
+                        },
+                        child: const Text('Forgot Password?',
+                            style: TextStyle(color: AppColors.primaryColor)),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24.0), // Spacer
+                  // Sign In Button
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Sign in clicked");
+                    },
+                    child: const Text('Sign in'),
+                  ),
+                  const SizedBox(height: 24.0), // Spacer
+                  // Sign Up Button
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Sign in With Google button pressed');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: AppColors.primaryColor,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/google_logo.png',
+                          width: 24.0,
+                          height: 24.0,
+                        ),
+                        const SizedBox(width: 8.0), // Spacer
+                        const Text('Sign in With Google'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 24.0), // Spacer
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don’t have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          print('Sign Up button pressed');
+                        },
+                        child: const Text('Sign Up',
+                            style: TextStyle(color: AppColors.primaryColor)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
