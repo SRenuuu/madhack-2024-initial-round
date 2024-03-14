4a0032f5fd87291d33825218ee7cff314c550237
kulasinghet <kulasinghet@gmail.com>
2024-03-15 01:15:23 +0530

    enable employer login and sign up

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/services/api_service.dart';
 import 'package:flutter_app/services/auth_service.dart';
 import 'package:flutter_app/services/resume_upload_service.dart';
+import 'package:flutter_app/views/auth/employer_signup_view.dart';
 import 'package:flutter_app/views/auth/login_view.dart';
+import 'package:flutter_app/views/auth/signup_view.dart';
 import 'package:flutter_app/views/home_view.dart';
-
+import 'package:flutter_app/views/job_listing_view.dart';
 import 'package:flutter_app/views/job_posting/job_post_form_view.dart';
 import 'package:flutter_app/views/resume_upload_view.dart';
-
-import 'package:flutter_app/views/job_listing_view.dart';
 import 'package:flutter_app/views/root_view.dart';
 import 'package:flutter_app/views/splash_view.dart';
-import 'package:get/get.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 
 import 'theme/app_theme.dart';
 
@@ -39,6 +39,8 @@ class MyApp extends StatelessWidget {
         routes: {
           '/root': (context) => const RootView(),
           '/login': (context) => const LoginView(),
+          '/sign_up': (context) => const SignUpView(),
+          '/employer_sign_up': (context) => const EmployerSignUpView(),
           '/home': (context) => const HomeView(),
           '/resume': (context) => const ResumeUploadView(),
           '/job_post': (context) => const JobPostFormView(),
--- flutter_app/lib/util/constants.dart
+++ flutter_app/lib/util/constants.dart
@@ -1,7 +1,8 @@
 class Constants {
   // API related data
-  static const String baseUrl = "http://127.0.0.1:4000/api/";
+  static const String baseUrl = "http://10.0.2.2:4000/api/";
 
   // Endpoints
   static const String loginEndpoint = "auth/signin";
+  static const String registerEndpoint = "auth/signup";
 }
