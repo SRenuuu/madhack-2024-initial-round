432378cdb1b9312863e6ca3b5be6396b4dc4df59
SRenuuu <sandulrenuja@gmail.com>
2024-03-16 10:58:09 +0530

    Add home view and controller

--- flutter_app/lib/controllers/home_controller.dart
+++ flutter_app/lib/controllers/home_controller.dart
@@ -1,7 +1,98 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 import '../services/auth_service.dart';
+import '../widgets/job_card.dart';
 
 class HomeController extends GetxController {
   final AuthService authService = Get.find<AuthService>();
+  final TextEditingController searchController = TextEditingController();
+
+  final RxBool isLoading = false.obs;
+  final RxBool isRecommendedJobPostsLoading = false.obs;
+  final RxBool isMostPopularJobPostsLoading = false.obs;
+  final RxBool isJobTagsLoading = false.obs;
+
+  final RxList<String> jobTagsList = RxList<String>([]);
+  final RxList<JobPosting> recommendedJobPosts = RxList<JobPosting>([]);
+  final RxList<JobPosting> mostPopularJobPosts = RxList<JobPosting>([]);
+
+  Future<void> fetchJobTags() async {
+    isJobTagsLoading.value = true;
+    await Future.delayed(const Duration(seconds: 2));
+    jobTagsList.value = [
+      "Software Engineer",
+      "Data Scientist",
+      "UX/UI Designer"
+    ];
+    isJobTagsLoading.value = false;
+  }
+
+  Future<void> fetchRecommendedJobPosts() async {
+    isRecommendedJobPostsLoading.value = true;
+    await Future.delayed(const Duration(seconds: 2));
+    recommendedJobPosts.value = [
+      JobPosting(
+        title: "Junior Web Developer",
+        location: "CodeSphere - Colombo, Sri Lanka",
+        description: "We are looking for a junior web developer...",
+        image:
+            "https://foyr.com/learn/wp-content/uploads/2021/08/modern-office-design.png",
+        salaryValue: "\$8K",
+        salaryFrequency: "Mo",
+        tags: ["Remote", "Full Time", "New"],
+        isSaved: true,
+      ),
+    ];
+    isRecommendedJobPostsLoading.value = false;
+  }
+
+  Future<void> fetchMostPopularJobPosts() async {
+    isMostPopularJobPostsLoading.value = true;
+    await Future.delayed(const Duration(seconds: 2));
+    mostPopularJobPosts.value = [
+      JobPosting(
+        title: "Junior Web Developer",
+        location: "CodeSphere - Colombo, Sri Lanka",
+        description: "We are looking for a junior web developer...",
+        image:
+            "https://foyr.com/learn/wp-content/uploads/2021/08/modern-office-design.png",
+        salaryValue: "\$8K",
+        salaryFrequency: "Mo",
+        tags: ["Remote", "Full Time", "New"],
+        isSaved: true,
+      ),
+      JobPosting(
+        title: "Junior Web Developer",
+        location: "CodeSphere - Colombo, Sri Lanka",
+        description: "We are looking for a junior web developer...",
+        image:
+            "https://foyr.com/learn/wp-content/uploads/2021/08/modern-office-design.png",
+        salaryValue: "\$8K",
+        salaryFrequency: "Mo",
+        tags: ["Remote", "Full Time", "New"],
+        isSaved: true,
+      ),
+      JobPosting(
+        title: "Junior Web Developer",
+        location: "CodeSphere - Colombo, Sri Lanka",
+        description: "We are looking for a junior web developer...",
+        image:
+            "https://foyr.com/learn/wp-content/uploads/2021/08/modern-office-design.png",
+        salaryValue: "\$8K",
+        salaryFrequency: "Mo",
+        tags: ["Remote", "Full Time", "New"],
+        isSaved: true,
+      ),
+    ];
+    isMostPopularJobPostsLoading.value = false;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchJobTags();
+    fetchRecommendedJobPosts();
+    fetchMostPopularJobPosts();
+  }
 }
--- flutter_app/lib/views/home_view.dart
+++ flutter_app/lib/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/controllers/home_controller.dart';
 import 'package:flutter_app/theme/colors.dart';
 import 'package:get/get.dart';
 
+import '../widgets/form_text_field.dart';
 import '../widgets/job_card.dart';
 
 class HomeView extends StatelessWidget {
@@ -10,65 +11,244 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    HomeController controller = Get.put(HomeController());
-
     return Scaffold(
+      appBar: buildAppBar(),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // Logo
-                Image.asset(
-                  'assets/images/workwise-logo.png',
-                  width: 240.0,
-                ),
-                const SizedBox(height: 8.0), // Spacer
-                const Text("Welcome to WorkWise",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
-                const SizedBox(height: 4.0), // Spacer
-                const Text("Your ultimate job search companion!",
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w300,
-                        color: WorkWiseColors.darkGreyColor)),
-                const SizedBox(height: 48.0), // Spacer
-                Obx(() => Column(
-                      children: [
-                        Text('Email: ${controller.authService.getUserEmail()}',
-                            style: const TextStyle(fontSize: 16.0)),
-                      ],
-                    )),
-                const SizedBox(height: 4.0), // Spacer
-                TextButton(
-                  onPressed: () {
-                    controller.authService.forceLogout();
-                  },
-                  child:
-                      const Text('Sign Out', style: TextStyle(fontSize: 16.0)),
-                ),
-                const SizedBox(height: 24.0), // Spacer
-                JobCard(
-                    onCardTap: () => Get.toNamed("/profile"),
-                    jobPosting: JobPosting(
-                        title: "Junior Web Developer",
-                        location: "CodeSphere - Colombo, Sri Lanka",
-                        description:
-                            "We are looking for a junior web developer, who is passionate about web development and has a keen eye for detail.",
-                        image:
-                            "https://foyr.com/learn/wp-content/uploads/2021/08/modern-office-design.png",
-                        salaryValue: "\$8K",
-                        salaryFrequency: "Mo",
-                        tags: ["Remote", "Full Time", "New"],
-                        isSaved: true))
-              ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildSearchField(),
+              const SizedBox(height: 24.0),
+              buildMostPopularJobs(),
+              buildJobTagsList(),
+              const SizedBox(height: 20.0),
+              buildRecommendedJobs(),
+              const SizedBox(height: 40.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    HomeController controller = Get.put(HomeController());
+    return AppBar(
+      backgroundColor: Colors.white,
+      toolbarHeight: 80.0,
+      title: buildAppBarTitle(),
+    );
+  }
+
+  Widget buildAppBarTitle() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            'assets/images/workwise-logo-long.png',
+            width: 180.0,
+          ),
+          buildProfileIcons(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProfileIcons() {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () => Get.toNamed("/profile"),
+          icon: const Icon(
+            Icons.bookmarks_rounded,
+            size: 24,
+            color: WorkWiseColors.tertiaryColor,
+          ),
+        ),
+        const SizedBox(width: 12.0),
+        buildProfileIconButton(),
+      ],
+    );
+  }
+
+  Widget buildProfileIconButton() {
+    return Container(
+      width: 42.0,
+      height: 42.0,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: WorkWiseColors.lightGreyColor,
+      ),
+      child: IconButton(
+        onPressed: () => Get.toNamed("/profile"),
+        icon: const Icon(
+          Icons.person,
+          size: 24,
+          color: WorkWiseColors.primaryColor,
+        ),
+      ),
+    );
+  }
+
+  Widget buildSearchField() {
+    HomeController controller = Get.put(HomeController());
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: formTextField(
+        showBorder: true,
+        isDense: true,
+        hintText: 'Search Jobs...',
+        showLabel: false,
+        label: 'Search Jobs',
+        prefixIcon: const Icon(Icons.search,
+            size: 26.0, color: WorkWiseColors.primaryColor),
+        suffixIcon: IconButton(
+          onPressed: () => print("Filter"),
+          icon: const Icon(Icons.filter_alt_rounded,
+              size: 26.0, color: WorkWiseColors.greyColor),
+        ),
+        controller: controller.searchController,
+        keyboardType: TextInputType.emailAddress,
+      ),
+    );
+  }
+
+  Widget buildMostPopularJobs() {
+    HomeController controller = Get.put(HomeController());
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Text(
+            "Most popular",
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
+          ),
+        ),
+        Obx(() => controller.isMostPopularJobPostsLoading.value
+            ? const Center(child: CircularProgressIndicator())
+            : controller.mostPopularJobPosts.isEmpty
+                ? const Center(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 16),
+                      child: Text(
+                        "No popular jobs found",
+                        style: TextStyle(
+                          color: WorkWiseColors.darkGreyColor,
+                        ),
+                      ),
+                    ),
+                  )
+                : SizedBox(
+                    height: 204.0,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: controller.mostPopularJobPosts.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          margin: EdgeInsets.only(
+                            top: 16.0,
+                            bottom: 24.0,
+                            left: index == 0 ? 24.0 : 0,
+                            right: index == 9 ? 24.0 : 16.0,
+                          ),
+                          child: JobCard(
+                            showDescription: false,
+                            shadowColor:
+                                WorkWiseColors.greyColor.withOpacity(0.5),
+                            onCardTap: () => Get.toNamed("/profile"),
+                            jobPosting: controller.mostPopularJobPosts[index],
+                          ),
+                        );
+                      },
+                    ),
+                  )),
+      ],
+    );
+  }
+
+  Widget buildJobTagsList() {
+    HomeController controller = Get.put(HomeController());
+    return Row(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Obx(
+              () => Row(
+                children: [
+                  const SizedBox(width: 16),
+                  ...controller.jobTagsList.map(
+                    (job) => Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Chip(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 8.0),
+                        label: InkWell(
+                          onTap: () => print("Job: $job"),
+                          child: Text(job),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        backgroundColor:
+                            WorkWiseColors.greyColor.withOpacity(0.5),
+                        labelStyle: const TextStyle(fontSize: 15.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                ],
+              ),
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  Widget buildRecommendedJobs() {
+    HomeController controller = Get.put(HomeController());
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Recommended for you",
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 16.0),
+          Obx(() => controller.isRecommendedJobPostsLoading.value
+              ? const Center(child: CircularProgressIndicator())
+              : controller.recommendedJobPosts.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "No recommended jobs found",
+                        style: TextStyle(
+                          color: WorkWiseColors.darkGreyColor,
+                        ),
+                      ),
+                    )
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: controller.recommendedJobPosts.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return JobCard(
+                          shadowColor:
+                              WorkWiseColors.greyColor.withOpacity(0.5),
+                          onCardTap: () => Get.toNamed("/profile"),
+                          jobPosting: controller.recommendedJobPosts[index],
+                        );
+                      },
+                    )),
+        ],
       ),
     );
   }
